d9eac2f98ff810c9ef8ab2db976380fb9a463703
_DevNote <rommelagasa102@gmail.com>
2026-03-01 18:21:44 +0800

    - submit a post request

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
 
+  <CreatePost />
   <PostList />
+  
 
 </template>
 
 <script>
 
 import PostList from './components/PostList.vue';
+import CreatePost from './components/CreatePost.vue';
 
 export default {
   name: 'App',
   components: {
-    PostList
+    PostList,
+    CreatePost
   }
 }
 </script>
---
+++ src/components/CreatePost.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <form @submit.prevent="createPost">
+            <div>
+                <label for="userId">Post User Id</label>
+                <input type="text" v-model="formData.userId" id="userId">
+            </div>
+            <div>
+                <label for="title">Post Title</label>
+                <input type="text" v-model="formData.title" id="title">
+            </div>
+            <div>
+                <label for="body">Post Body</label>
+                <input type="text" v-model="formData.body" id="body">
+            </div>
+            <button type="submit">Create Post</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        name: 'CreatePost',
+        data() {
+            return {
+                formData: {
+                    userId: '',
+                    title: '',
+                    body: ''
+                }
+            }
+        },
+        methods: {
+            createPost(){
+                axios.post(
+                    'https://jsonplaceholder.typicode.com/posts', 
+                    this.formData)
+                    .then(response => {
+                        console.log(response.data);
+                    }).catch(error => {
+                        console.error(error);
+                    });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+#create-post{
+    margin-top: 30px;
+}
+</style>
